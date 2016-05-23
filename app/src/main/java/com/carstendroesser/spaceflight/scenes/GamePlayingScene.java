006e722c23eda39d3b48bdc98f6373f59850518a
Carsten Drösser <carsten.droesser@gmail.com>
2016-05-23 14:32:42 +0200

    show spaceship

--- app/src/main/java/com/carstendroesser/spaceflight/scenes/GamePlayingScene.java
+++ app/src/main/java/com/carstendroesser/spaceflight/scenes/GamePlayingScene.java
@@ -1,5 +1,6 @@
 package com.carstendroesser.spaceflight.scenes;
 
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
@@ -13,14 +14,30 @@ import static org.andengine.entity.scene.background.ParallaxBackground.ParallaxE
 public class GamePlayingScene extends BaseScene {
 
     private AutoParallaxBackground mParallaxBackground;
+    private Sprite mSpaceship;
 
     @Override
     public void create() {
         mEngine.registerUpdateHandler(new FPSLogger());
 
         mParallaxBackground = new AutoParallaxBackground(0, 0, 0, 40);
-        mParallaxBackground.attachParallaxEntity(new ParallaxEntity(-5.0f, new Sprite(0, SCREEN_HEIGHT - mResourceManager.mParallaxBackground.getHeight(), mResourceManager.mParallaxBackground, mVertexBufferObjectManager)));
+        mParallaxBackground.attachParallaxEntity(
+                new ParallaxEntity(
+                        -5.0f,
+                        new Sprite(0,
+                                SCREEN_HEIGHT - mResourceManager.mParallaxBackground.getHeight(),
+                                mResourceManager.mParallaxBackground,
+                                mVertexBufferObjectManager)));
         setBackground(mParallaxBackground);
+
+        mSpaceship = new Sprite(
+                SCREEN_WIDTH / 2 - mResourceManager.mSpaceshipTextureRegion.getWidth() / 2,
+                SCREEN_HEIGHT / 2 - mResourceManager.mSpaceshipTextureRegion.getHeight() / 2,
+                mResourceManager.mSpaceshipTextureRegion,
+                mVertexBufferObjectManager);
+        mSpaceship.setZIndex(10);
+        attachChild(mSpaceship);
+
     }
 
     @Override
